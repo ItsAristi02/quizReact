86c1aed58387dc4c06bf8ceb15105af1d858810d
ItsAristi02 <alejandraaristizabal49@gmail.com>
2022-11-24 17:00:59 -0500

    Se crea formulario de productos

--- src/components/helpers/Aside.js
+++ src/components/helpers/Aside.js
@@ -2,6 +2,16 @@ const Aside = () => {
     return (
         <aside>
             
+            <form id="formulario" action="">
+                    <h1 className="titulo">PRODUCTOS</h1>
+                <input className="form_item" type="text" placeholder="Nombre" />
+                <input className="form_item" type="text" placeholder="Apellido" />         
+                <input className="form_item" type="text" placeholder="Documento" />
+                <input className="form_item" type="text" placeholder="Nota Uno" />
+                <input className="form_item" type="text" placeholder="Nota Dos" />
+                <input id="btnCalcular" type="submit" value="Guardar"/>
+            
+             </form>
         </aside>
     )
 }
